b736c594a8202e2b66d0f669c6d4376dbd1b165a
pfalz <mega75581@gmail.com>
2023-09-07 14:11:09 +0900

    added css file for styling

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,15 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return (<h2>Expense item!</h2>);
+  return (
+    <div className="expense-item">
+      <div>September 9th 2023</div>
+      <div className="expense-item__description">
+        <h2>Car insurance</h2>
+        <div className="expense-item__price">$500</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
