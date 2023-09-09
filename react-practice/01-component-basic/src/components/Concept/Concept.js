70bdec3d82242f63d7080bd04fe4ad8a7f7f0d6d
pfalz <mega75581@gmail.com>
2023-09-09 14:05:28 +0900

    finished 01-component-basic practice

---
+++ react-practice/01-component-basic/src/components/Concept/Concept.js
@@ -0,0 +1,11 @@
+function Concept(props) {
+  return (
+    <li className="concept">
+      <img src={props.concept.image} alt={props.concept.title} />
+      <h2>{props.concept.title}</h2>
+      <p>{props.concept.description}</p>
+    </li>
+  );
+}
+
+export default Concept;
