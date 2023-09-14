87ca369f6232b75f4bcd332143d51c0e26819d6c
pfalz <mega75581@gmail.com>
2023-09-14 16:01:28 +0900

    outputting lists by filtered year

--- 01-react-basic/src/components/Expenses/Expenses.js
+++ 01-react-basic/src/components/Expenses/Expenses.js
@@ -12,13 +12,17 @@ function Expenses(props) {
     setFilteredYear(year);
   };
 
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selectedYear={filteredYear}
         onChangeFilter={filterHandler}
       />
-      {props.items.map((expense) => (
+      {filteredExpenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
           title={expense.title}
