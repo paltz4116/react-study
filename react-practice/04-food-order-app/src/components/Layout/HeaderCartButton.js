22af44fb574b5f0b17452c040e2bcff5104ecc61
pfalz <mega75581@gmail.com>
2023-11-08 16:13:52 +0900

    adding Cart reducer

--- react-practice/04-food-order-app/src/components/Layout/HeaderCartButton.js
+++ react-practice/04-food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
+import { useContext } from "react";
+
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numOfCartItems = cartCtx.items.reduce((currNum, item) => {
+    return currNum + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{numOfCartItems}</span>
     </button>
   );
 };
