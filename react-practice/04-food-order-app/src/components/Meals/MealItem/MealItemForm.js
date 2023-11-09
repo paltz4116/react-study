ca396b92d3b40e8db0fdfe7c6094aa98297ad5ee
pfalz <mega75581@gmail.com>
2023-11-09 14:38:51 +0900

    using useRef for add item to cart

--- react-practice/04-food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ react-practice/04-food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,34 @@
+import { useRef, useState } from "react";
+
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+  const [amountIsValid, setAmountValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNum = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNum < 1 ||
+      enteredAmountNum > 5
+    ) {
+      setAmountValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNum);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +40,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (1~5).</p>}
     </form>
   );
 };
