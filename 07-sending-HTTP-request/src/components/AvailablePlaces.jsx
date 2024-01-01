bbe04833dd716b57f1e4482e00bf8228f57fe230
pfalz <mega75581@gmail.com>
2024-01-01 15:09:26 +0900

    Fetched data conversion

--- 07-sending-HTTP-request/src/components/AvailablePlaces.jsx
+++ 07-sending-HTTP-request/src/components/AvailablePlaces.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetchting, setIsFectching] = useState();
@@ -20,14 +21,21 @@ export default function AvailablePlaces({ onSelectPlace }) {
           throw new Error("Failed to fetch places");
         }
 
-        setAvailablePlaces(resData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            resData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFectching(false);
+        });
       } catch (error) {
         setError({
           message: error.message || "Could not fetch places",
         });
+        setIsFectching(false);
       }
-
-      setIsFectching(false);
     }
 
     fectchPlaces();
